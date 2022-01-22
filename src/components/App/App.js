97038af047ad1c99a42e52474f5ebf4cb177a201
Valerii Vitenko <valero2313@gmail.com>
2022-01-22 17:37:28 +0200

    removed lib code to avoid conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
-import Webcam from 'react-webcam';
 import { toast, ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 
 function App() {
   const [video, setVideo] = useState(null);
-  const [photo, setPhoto] = useState(null);
   const [mode, setMode] = useState('environment');
+  
   const videoRef = useRef(null);
-  const cameraRef = useRef(null);
 
   useEffect(() => {
     getVideo();
   }, [videoRef, mode]);
-
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: mode,
-  };
 
   const getVideo = () => {
     navigator.mediaDevices
@@ -60,32 +52,15 @@ function App() {
     setMode(mode === 'user' ? 'environment' : 'user');
   };
 
-  const getScreenshot = () => {
-    const screenshot = cameraRef.current.getScreenshot({
-      width: 800,
-      height: 600,
-    });
-    setPhoto(screenshot);
-  };
-
-  return <div>
-  <div className="streamWrapper">
+  return (
+    <div>
+      <div className="streamWrapper">
         <video ref={videoRef} />
-        // <Webcam
-        //   ref={cameraRef}
-        //   videoConstraints={videoConstraints}
-        //   imageSmoothing="true"
-        //   screenshotQuality="1"
-        // />
-        // {photo && <img src={photo} alt="screen snap" />}
       </div>
       <div className="btnWrapper">
         <button className="btn" onClick={getVideo}>
           START
         </button>
-        // <button className="btn" onClick={getScreenshot}>
-        //   PHOTO
-        // </button>
         <button className="btn" onClick={stopVideo}>
           STOP
         </button>
@@ -97,7 +72,9 @@ function App() {
         transition={Zoom}
         autoClose={4000}
         toastStyle={{ backgroundColor: '#c57d7d', color: '#000000' }}
-      /></div>;
+      />
+    </div>
+  );
 }
 
 export default App;
