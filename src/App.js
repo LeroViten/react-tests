216768554cd317526c7960b5e3e66896a7edf362
Valerii Vitenko <valero2313@gmail.com>
2022-05-12 23:21:29 +0300

    added custom Loader component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import useFetch from './hooks/useFetch';
 import Comments from './components/Comments';
+import Loader from './components/Loader';
 import './components/Comments/Comments.scss';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      {loading && <h2>LOADING...</h2>}
+      {loading && <Loader/>}
       {error && <h2>{error}</h2>}
       <a className="hoverBtn" href="https://www.google.com" target="_blank" rel="noreferrer">
         <span>Get Started</span>
